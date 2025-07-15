459f90af064f22e347e172067374f9cce8fab5be
caioandrademota <caiomota26@gmail.com>
2025-07-15 17:31:39 -0400

    Novo repositorio

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,21 @@
+//2 parametros (funcionalidade)
+describe('Login', () => {
+
+    //caso de teste
+    it('Login com sucesso', () =>{
+
+    })
+
+    // //caso de teste
+    // it('E-mail invalido', () =>{
+        
+    // })
+
+    // //caso de teste
+    // it('Senha Invalida', () =>{
+        
+    // })
+
+
+
+})
