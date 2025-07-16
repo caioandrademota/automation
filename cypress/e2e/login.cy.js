e6e0bd2e65df8556185010d9bc7334acb72c696f
caioandrademota <caiomota26@gmail.com>
2025-07-16 12:40:06 -0400

    Set baseURL and using selection methods in Cypress

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,8 +2,23 @@
 describe('Login', () => {
 
     //caso de teste
-    it('Login com sucesso', () =>{
+    it('Successful Login', () =>{
+        //comando para acesso de URL, relacionado a baseURL
+        //get
+        cy.visit('/')
+            .get('.header-logo') //element selection method
 
+        //contains
+        cy.get('#top_header').as('header')// as() sets here
+            .contains('Login') //find elements by text, after using get to a smaller search area
+
+        //find
+        cy.get('#top_header')
+            .find('.fa-user') //find elements - more general
+
+        //as
+        cy.get('@header') //use with @
+            .find('.fa-user') //it provides a easier way invoke your elements
     })
 
     // //caso de teste
